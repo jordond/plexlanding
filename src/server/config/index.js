472b4723c7a09b3c819af895d4a2c57d36246e55
Jordon de Hoog <jordon.dehoog@gmail.com>
2016-06-04 19:05:16 -0400

    Config config module to a class

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,38 +1,46 @@
-import { merge, isEmpty } from 'lodash';
+import { merge } from 'lodash';
 
 import defaultConfig from './defaults';
 import User from './user';
 import { development, production } from './environments';
 
-let fullConfig = {};
-
-export function defaults() {
-  const environment = defaultConfig.env === 'production' ? production : development;
-  return merge({}, defaultConfig, environment);
-}
-
-/**
- * Merge the main config files, and load the user config from data dir
- *
- * @param boolean reload  Force a reload of the entire config
- * @returns Promise<object>
- */
-export function load(reload = false) {
-  if (!isEmpty(fullConfig) && !reload) {
-    return Promise.resolve(fullConfig);
+export class Config {
+  constructor() {
+    this.config = this.defaults();
+    this.userConfigLoaded = false;
   }
 
-  const userConfig = new User();
-  return userConfig.read().then((config = {}) => {
-    fullConfig = merge(defaults(), config);
-    return fullConfig;
-  });
+  defaults() {
+    const environment = defaultConfig.env === 'production' ? production : development;
+    return merge({}, defaultConfig, environment);
+  }
+
+  /**
+   * Merge the main config files, and load the user config from data dir
+   *
+   * @param boolean reload  Force a reload of the entire config
+   * @returns Promise<object>
+   */
+  load(reload = false) {
+    if (this.userConfigLoaded && !reload) {
+      return Promise.resolve(this.config);
+    }
+
+    const userConfig = new User();
+    return userConfig.read().then((config = {}) => {
+      this.config = merge(this.config, config);
+      this.userConfigLoaded = true;
+      return this.config;
+    });
+  }
+
+  save(config = {}) {
+    const userConfig = new User();
+    this.config = merge(this.config, config);
+    return userConfig.update(config);
+  }
+
 }
 
-export function save(config = {}) {
-  const userConfig = new User();
-  fullConfig = merge(fullConfig, config);
-  return userConfig.update(config);
-}
 
-export default { load, defaults, User, save };
+export default new Config;
